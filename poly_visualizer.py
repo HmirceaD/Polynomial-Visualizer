be1ff57747f519b6827071f64664023a6ac524bf
Mircea Dan <mircea_dan97@yahoo.com>
2020-01-15 13:50:24 +0200

    added generalization

--- poly_visualizer.py
+++ poly_visualizer.py
@@ -18,9 +18,10 @@ bot_frame = bot_frame.pack(side=tk.BOTTOM)
 
 canvas = tk.Canvas(top_frame, bg="white", height=500, width=1300)
 
+final_coords = []
 
 class Step:
-
+    # class that represent a current step for each polynomial coefficient displayed on the board
     def __init__(self, coef):
         self.xin = None
         self.xout = None
@@ -33,11 +34,11 @@ class Step:
 
 
 for i in range(len(COEFFS)):
-    current_steps.append(Step(COEFFS[i]))
+    current_steps.append(Step(COEFFS[i])) # create coefficients list
 
 
 def calc_step():
-
+    # CALCULATE THE NEXT STEP OF THE ALGORITHM (GETS CALLED ON BUTTON CLICK)
     if current_steps[len(current_steps) - 1].pout is not None:
         OUTPUT_POLY.append(current_steps[len(current_steps) - 1].pout)
 
@@ -53,9 +54,10 @@ def calc_step():
         else:
             current_steps[i].pout = None
 
-        i = i - 1
+        i -= 1
 
     if INPUT_POLY:
+
         aux = INPUT_POLY.pop()
         current_steps[0].xin = aux
         current_steps[0].xout = aux
@@ -68,14 +70,15 @@ def calc_step():
         current_steps[0].pin = None
         current_steps[0].pout = None
 
-    canvas.delete("tag")
+    canvas.delete("DELETEME")
 
-    draw_nums(canvas, app, 350, 150, current_steps[0].pin, current_steps[0].pout,
-              current_steps[0].xin, current_steps[0].xout)
-    draw_nums(canvas, app, 560, 150, current_steps[1].pin, current_steps[1].pout,
-              current_steps[1].xin, current_steps[1].xout)
-    draw_nums(canvas, app, 770, 150, current_steps[2].pin, current_steps[2].pout,
-              current_steps[2].xin, current_steps[2].xout)
+    cont = 0
+    for i in COEFFS:
+        draw_nums(canvas, app, final_coords[cont][0], final_coords[cont][1],
+                  current_steps[cont].pin,current_steps[cont].pout,
+              current_steps[cont].xin, current_steps[cont].xout)
+        cont += 1
+
     draw_lists()
 
 
@@ -92,29 +95,29 @@ def list_to_string(list1):
 
 def draw_nums(canvas, root, x, y, pin, pout, xin, xout):
 
-    # top left
+    # top left pin value
     if pin is not None:
-        canvas.create_text((x - 15, y + 10), tag="tag", text=str(pin), font="Times 16")
+        canvas.create_text((x - 15, y + 10), tag="DELETEME", text=str(pin), font="Times 16")
     else:
-        canvas.create_text((x - 15, y + 10), tag="tag", text="", font="Times 16")
+        canvas.create_text((x - 15, y + 10), tag="DELETEME", text="", font="Times 16")
 
-    # top right
+    # top right pout value
     if pout is not None:
-        canvas.create_text((x + 115, y + 10), tag="tag", text=str(pout), font="Times 16")
+        canvas.create_text((x + 115, y + 10), tag="DELETEME", text=str(pout), font="Times 16")
     else:
-        canvas.create_text((x + 115, y + 10), tag="tag", text="", font="Times 16")
+        canvas.create_text((x + 115, y + 10), tag="DELETEME", text="", font="Times 16")
 
-    # bot left
+    # bot left input value
     if xin is not None:
-        canvas.create_text((x - 15, y + 170), tag="tag", text=str(xin), font="Times 16")
+        canvas.create_text((x - 15, y + 170), tag="DELETEME", text=str(xin), font="Times 16")
     else:
-        canvas.create_text((x - 15, y + 170), tag="tag", text="", font="Times 16")
+        canvas.create_text((x - 15, y + 170), tag="DELETEME", text="", font="Times 16")
 
-    # bot right
+    # bot right output value
     if xout is not None:
-        canvas.create_text((x + 115, y + 170), tag="tag", text=str(xout), font="Times 16")
+        canvas.create_text((x + 115, y + 170), tag="DELETEME", text=str(xout), font="Times 16")
     else:
-        canvas.create_text((x + 115, y + 170), tag="tag", text="", font="Times 16")
+        canvas.create_text((x + 115, y + 170), tag="DELETEME", text="", font="Times 16")
 
 
 def draw_rectangle(canvas, top_frame, P, poly, x, y, hurdle = False):
@@ -129,17 +132,18 @@ def draw_rectangle(canvas, top_frame, P, poly, x, y, hurdle = False):
     canvas.create_line(top_frame, x+100, y+20, x+150, y+20)
     canvas.create_line(top_frame, x+100, y+180, x+150, y+180)
 
+    # Draw P and polynomial value
     canvas.create_text(top_frame, x+50, y+30, text = P, font = "Times 20")
     canvas.create_text(top_frame, x+50, y+150, text = str(poly), font = "Times 20")
 
     if hurdle:
-        #top left
+        #top left black square
         canvas.create_rectangle(x-60, y+30, x-50, y+10, fill="black", outline='black')
-        #bot left
+        #bot left black square
         canvas.create_rectangle(x - 60, y + 190, x - 50, y + 170, fill="black", outline='black')
-        #top right
+        #top right black square
         canvas.create_rectangle(x + 150, y + 30, x + 160, y + 10, fill="black", outline='black')
-        #bot right
+        #bot right black square
         canvas.create_rectangle(x + 150, y + 190, x + 160, y + 170, fill="black", outline='black')
 
 
@@ -152,11 +156,18 @@ def draw_board(app):
     label = tk.Label(app, text = "Polynomial Visualizer (x^2 + 2x + 1)")
     label.pack()
 
-    draw_rectangle(canvas, top_frame, "P0", 1, 350, 150)
+    init_coords = [350, 150]
+    cont = 0
+    for i in COEFFS:
+        if i == COEFFS[0] or i == COEFFS[-1]:
+            isHurdle = False
+        else:
+            isHurdle = True
 
-    draw_rectangle(canvas, top_frame, "P1", 2, 560, 150, hurdle = True)
-
-    draw_rectangle(canvas, top_frame, "P2", 1, 770, 150)
+        draw_rectangle(canvas, top_frame, "P"+str(cont),COEFFS[cont], init_coords[0], init_coords[1], hurdle=isHurdle)
+        final_coords.append((init_coords[0], init_coords[1]))
+        init_coords[0] += 210
+        cont += 1
 
     #Input Text
     canvas.create_text(top_frame, 100, 50, text="Input", font="Times 20")
@@ -165,17 +176,17 @@ def draw_board(app):
 
     draw_lists()
 
-    step_button = tk.Button(bot_frame, text="Advance Algorithm", command=on_click)
+    step_button = tk.Button(bot_frame, text="Next Step", command=on_click)
 
     canvas.pack()
     step_button.pack()
 
 
 def draw_lists():
-    # Input Poly
-    canvas.create_text(top_frame, 100, 90, text=list_to_string(INPUT_POLY), font="Times 16", tag="tag")
-    # Output Poly
-    canvas.create_text(top_frame, 1100, 90, text=list_to_string(OUTPUT_POLY), font="Times 16", tag="tag")
+    # Input Values
+    canvas.create_text(top_frame, 100, 90, text=list_to_string(INPUT_POLY), font="Times 16", tag="DELETEME")
+    # Output Values
+    canvas.create_text(top_frame, 1100, 90, text=list_to_string(OUTPUT_POLY), font="Times 16", tag="DELETEME")
 
 
 def start_visual():
